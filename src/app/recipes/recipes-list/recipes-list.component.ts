71acfc9c2eb96b0b4830f6ab67cda6b05454d47b
Mohammad Alkayyali <M.Alkayyali@eska.com>
2024-01-18 16:51:34 +0300

    update

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.module';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes:Recipe[]=[
+  new Recipe("this is a test recipe","this is just a test ","https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=556,505")
+]
   constructor() { }
 
   ngOnInit(): void {
